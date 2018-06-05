f4966eb4031b57f0a10860e4351df13369e7bfe1
tmt <thomas.culdaut@gmail.com>
2018-06-05 09:31:23 +0200

    start now

---
+++ src/api/index.js
@@ -0,0 +1,1 @@
+fetchPikachu = () => null
---
+++ src/component/Pikachu.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Pikachu extends Component {
+  render() {
+    return <div>Pikachu</div>;
+  }
+}
+
+export default Pikachu;
